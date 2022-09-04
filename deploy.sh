f3de308759ea21d76d9e2265a591958b6a920fa1
rodrigo araújo <rodrigo.dev99@gmail.com>
2022-09-04 19:09:33 -0300

    test

--- deploy.sh
+++ deploy.sh
@@ -13,14 +13,14 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git checkout -b main
+git checkout -b gh-pages
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
+git push -f git@github.com:rodrigo-97/easy-nup-front.git main:gh-pages
 
 cd -
